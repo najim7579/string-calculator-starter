3cdbce476f2c798441d9735a76620c2bef255d87
najim7579 <najimshikalgar7579@gmail.com>
2021-10-17 00:57:35 +0530

    three

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -24,40 +24,12 @@ public class StringCalculatorShould {
         assertEquals(stringCalculator.add("5"), 5);
     }
     
-  /*  @Test
+    @Test
     public void numbersCommaDelimitedShouldBeSummed() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(stringCalculator.add("1,2"), 3);
         assertEquals(25, stringCalculator.add("10,15"));
     }
 
-    @Test
-    public void numbersNewlineDelimitedShouldBeSummed() {
-    	 StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(stringCalculator.add("1\n2"), 3);
-        assertEquals(stringCalculator.add("11\n13"), 24);
-    }
-
-    @Test
-    public void threeNumbersDelimitedAnywayShouldBeSummed() {
-    	 StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(stringCalculator.add("1,2,3"), 6);
-        assertEquals(stringCalculator.add("5\n2\n3"), 10);
-    }
-    
-    @Test
-    public void negativeInputReturnsException() {
-    	 StringCalculator stringCalculator = new StringCalculator();
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Negative input!");
-        stringCalculator.add("-1");
-        stringCalculator.add("-5,10\n-15");
-    }
-    @Test
-    public void numbersGreaterThan1000AreIgnored() {
-    	 StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(stringCalculator.add("5,12,1001"), 17);
-        assertEquals(stringCalculator.add("14124,22\n4,1214"), 26);
-    }*/
-	
+  
 }
