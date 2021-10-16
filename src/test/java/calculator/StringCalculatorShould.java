3178df5985952a73fb5cfc97436dca94dd54a6b4
najim7579 <najimshikalgar7579@gmail.com>
2021-10-17 00:59:38 +0530

    five

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -38,7 +38,22 @@ public class StringCalculatorShould {
         assertEquals(stringCalculator.add("11\n13"), 24);
     }
 
-   
+    @Test
+    public void threeNumbersDelimitedAnywayShouldBeSummed() {
+    	 StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(stringCalculator.add("1,2,3"), 6);
+        assertEquals(stringCalculator.add("5\n2\n3"), 10);
+    }
+    
+    @Test
+    public void negativeInputReturnsException() {
+    	 StringCalculator stringCalculator = new StringCalculator();
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Negative input!");
+        stringCalculator.add("-1");
+        stringCalculator.add("-5,10\n-15");
+    }
+  
 
   
 }
